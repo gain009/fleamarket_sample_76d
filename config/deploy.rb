b7b640e958f568887328d0f9c86aba6c3fc0e200
AIKIHIGASHI <sheltie0805@gmail.com>
2020-06-03 15:12:06 +0900

    thaad_commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.0'
 
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'fleamarket_sample_76d'
-set :repo_url,  'AIKIHIGASHI/fleamarket_sample_76d.git'
+set :repo_url,  'git@github.com:AIKIHIGASHI/fleamarket_sample_76d.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
