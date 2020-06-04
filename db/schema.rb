3b368c37f7fddce195fb14ca6f09f26380bb6fea
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-04 18:21:22 +0900

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_06_04_050153) do
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
