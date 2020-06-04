f3461090d9acfd07a154d57b41793d00c878b3d9
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-04 18:19:31 +0900

    confulict

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,29 @@
 
 ActiveRecord::Schema.define(version: 2020_06_04_050153) do
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
