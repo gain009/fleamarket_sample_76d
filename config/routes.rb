7ec02ecd622cd9be806c5984e87e0a2ac02ccc9b
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-05 10:14:40 +0900

    login-for-items-page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  resources :user
 
   resources :items, only: :show do
     get "buy/confirmation", to: 'items#buy_confirmation'
