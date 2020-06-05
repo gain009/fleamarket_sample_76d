ee242bf0b9979ad0271b6e77ce3ee547552041cb
Ataru Oniki <ataru19980606@gmail.com>
2020-06-05 15:53:59 +0900

    add validates destination.rb
    Destinationモデルにバリデーションを追加しました。

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,12 @@
 class Destination < ApplicationRecord
+  validates :destination_family_name, presence: true
+  validates :destination_first_name, presence: true
+  validates :destination_family_name_kana, presence: true
+  validates :destination_first_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :user_id, presence: true
+
 end
