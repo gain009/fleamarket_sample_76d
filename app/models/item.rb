33e6b631a1861ca52e2dc6ed5249c49fe5373721
Ataru Oniki <ataru19980606@gmail.com>
2020-06-05 15:52:06 +0900

    add validates item.rb
    Itemモデルにバリデーションを追加しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
+  validates :size, presence: true
+  validates :shipping_area, presence: true
+  validates :shipping_method, presence: true
+  validates :shipping_date, presence: true
+  validates :prefecture_id, presence: true
+  validates :category_id, presence: true
+  validates :brand_id, presence: true
+  validates :user_id, presence: true
+
 end
