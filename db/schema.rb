463b544c3040f3541787a4dbdeeb49489e869e00
Ataru Oniki <ataru19980606@gmail.com>
2020-06-05 17:51:15 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_020148) do
     t.string "prefecture_id", null: false
     t.string "judgment"
     t.integer "category_id", null: false
-    t.integer "brand_id", null: false
+    t.integer "brand_id"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
