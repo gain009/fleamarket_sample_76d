38a4c86d61f61f31194b31a5add9543ade1302ab
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-05 11:47:13 +0900

    table_user

--- db/migrate/20200604050153_devise_create_users.rb
+++ db/migrate/20200604050153_devise_create_users.rb
@@ -21,7 +21,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
 
 
-
       ## Recoverable
       # t.string   :reset_password_token
       # t.datetime :reset_password_sent_at
@@ -47,8 +46,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # t.timestamps null: false
     end
+
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
