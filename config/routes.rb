f6d9916e9e391b93f1ac81d52c283dc21220c53d
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-08 09:17:13 +0900

    items_controllerに@itemsを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :user
 
-  resources :items, only: :show do
+  resources :items, only: [:index, :show] do
     get "buy/confirmation", to: 'items#buy_confirmation'
   end
   
