9258842517ec9ad61c5bdcf9f145f5a87406d329
toyasu <64839788+toyasu@users.noreply.github.com>
2020-06-08 10:09:54 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   devise_for :users
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -10,5 +12,13 @@ Rails.application.routes.draw do
 
   get 'mypage/index', to: 'mypage#index'
   get 'logout/index', to: 'logout#index'
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 
 end
