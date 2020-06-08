786be57f36520baeb38c019d8424e85af38cb9b3
Ataru Oniki <ataru19980606@gmail.com>
2020-06-08 22:51:32 +0900

    Update item.rb
    active_hashを使用してアソシエーションを結びました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_method
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_date
 
   validates :name, presence: true
   validates :price, presence: true
@@ -17,4 +20,5 @@ class Item < ApplicationRecord
   has_many :images
   belongs_to :user
   belongs_to :category
+  belongs_to :brand
 end
