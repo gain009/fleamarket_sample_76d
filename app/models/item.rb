06dd36b493a6a3f2dfb64808f581a57cdebba786
Ataru Oniki <ataru19980606@gmail.com>
2020-06-08 23:16:42 +0900

    Add association for active_hash
    active_hashを利用するためのアソシエーションをitemモデルに記述しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_method
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_date
 
   validates :name, presence: true
   validates :price, presence: true
