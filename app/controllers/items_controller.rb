93d0a6a9a173051f406555092f64b8dae7e4fb29
Ataru Oniki <ataru19980606@gmail.com>
2020-06-08 17:27:05 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @same_user_items = Item.where(user_id: @item.user.id).order(created_at: "DESC").limit(3)
+    @same_user_items = Item.where(user_id: @item.user_id).order(created_at: "DESC").limit(3)
     @same_category_items = Item.where(category_id: @item.category_id).order(created_at: "DESC").limit(3)
   end
 
