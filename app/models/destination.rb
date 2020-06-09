9bcc00362d01a307b93d56c7c134deee0bf351c7
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-09 12:05:29 +0900

    validate

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,8 +1,8 @@
 class Destination < ApplicationRecord
-  validates :destination_family_name, presence: true
-  validates :destination_first_name, presence: true
-  validates :destination_family_name_kana, presence: true
-  validates :destination_first_name_kana, presence: true
+  validates :destination_family_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "全角のみ使えます" }
+  validates :destination_first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "全角のみ使えます" }
+  validates :destination_family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみ使えます" }
+  validates :destination_first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみ使えます" }
   validates :post_code, presence: true
   validates :prefecture, presence: true
   validates :city, presence: true
