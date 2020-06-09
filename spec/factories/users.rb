e00c136f7bc03679184b9f3581c3c0a6264b60c8
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-09 18:07:06 +0900

    test__code

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  
+  factory :user do
+    nickname                {"Ferumi"}
+    first_name              {"山田"}
+    family_name               {"太郎"}
+    first_name_kana         {"ヤマダ"}
+    family_name_kana          {"タロウ"}
+    email                   {"ferumi@com"}
+    password                {"pass1234"}
+    birth_day               {"2020-02-21"}
+  end
+end
