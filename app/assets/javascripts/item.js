7051fef3c88e4d4e04ebae95b0d73068dca89715
Ataru Oniki <ataru19980606@gmail.com>
2020-06-10 22:05:46 +0900

    add open a popup method
    非表示にしていたポップアップが表示される関数を定義しました。

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -3,5 +3,9 @@ $(function () {
 // 商品削除ポップアップ機能
   var deleteBtn = $(".delete-button");
   var deletePopUp = $("#delete-popup");
+  // ポップアップを開く
+  $("#open-popup").click(function(e) {
+    deletePopUp.fadeIn(300);
+  });
 
 });
