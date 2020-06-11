fabda12c6788d4d4c0bf15dadbe4a36ab15c6d55
o-taka-yuma <64815738+o-taka-yuma@users.noreply.github.com>
2020-06-11 18:16:02 +0900

    routing__collection__json

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user
   resources :items, only: [:index, :new, :show, :destroy] do
-
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
 
     get "buy/confirmation", to: 'items#buy_confirmation'
   end
