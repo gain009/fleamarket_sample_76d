158cbd568c26c8f22f0beaa35caff8bc498ce660
Ataru Oniki <ataru19980606@gmail.com>
2020-06-12 18:28:25 +0900

    add items#update logic
    updateアクションの中身を記述しました。
    成功時ホーム画面へ遷移し、失敗時再度編集画面へ遷移するようにしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,13 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      flash[:notice] = "変更に失敗しました"
+      render :edit
+    end
   end
 
   def show
