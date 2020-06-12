30dac7ce9845557c137b00a0de1e93e6ddba9d9f
Ataru Oniki <ataru19980606@gmail.com>
2020-06-12 17:47:30 +0900

    add routing of items create, edit, update
    itemsコントローラーのcreate, edit, updateアクションへのルーティングを作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user
-  resources :items, only: [:index, :new, :show, :edit, :update, :destroy] do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     get "buy/confirmation", to: 'items#buy_confirmation'
   end
 
