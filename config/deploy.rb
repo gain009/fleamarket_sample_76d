6bdd9eecac06b1cb7d091717f8b1f3c9ab8e64e6
AIKIHIGASHI <sheltie0805@gmail.com>
2020-06-12 13:05:36 +0900

    本番とローカルで画像の保存先変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 set :linked_files, %w{ config/master.key }
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -34,6 +35,20 @@ namespace :deploy do
         execute "mkdir -p #{shared_path}/config"
       end
       upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+
+
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
   before :starting, 'deploy:upload'
