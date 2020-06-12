3868cebba77b3065fa415ea0cbc70a6f94bc605b
Ataru Oniki <ataru19980606@gmail.com>
2020-06-12 18:30:10 +0900

    remove buyer_id in strong parameter
    ストロングパラメーターにbuyer_idが含まれていましたが、フォームで送られてくる情報ではないので削除しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -68,9 +68,7 @@ class ItemsController < ApplicationController
 
   
   private
-
   def item_params
-    
-    params.require(:item).permit(:name, :price, :description, :category_id, :size_id, :status_id, :shipping_cost_id, :shipping_method_id, :prefecture_id, :shipping_date_id, :brand_id, :buyer_id,  images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :description, :category_id, :size_id, :status_id, :shipping_cost_id, :shipping_method_id, :prefecture_id, :shipping_date_id, :brand_id, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
 end
