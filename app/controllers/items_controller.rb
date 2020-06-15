908ae57ff4850ec96558c278f155986aa9846d89
Ataru Oniki <ataru19980606@gmail.com>
2020-06-15 17:50:42 +0900

    fixed conflict
    masterでの変更に適応しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       @item = Item.new
