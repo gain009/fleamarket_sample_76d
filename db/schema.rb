a031489ebc696f9529dbac25adb4bb3f291e9505
AIKIHIGASHI <sheltie0805@gmail.com>
2020-06-17 10:11:38 +0900

    【修正】.url追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_10_050938) do
-
 
   create_table "brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -49,8 +47,8 @@ ActiveRecord::Schema.define(version: 2020_06_10_050938) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "destination_family_name_kana", null: false
     t.integer "prefecture_id", null: false
+    t.string "destination_family_name_kana", null: false
     t.index ["user_id"], name: "index_destinations_on_user_id"
   end
 
@@ -83,6 +81,12 @@ ActiveRecord::Schema.define(version: 2020_06_10_050938) do
     t.index ["user_id"], name: "fk_rails_d4b6334db2"
   end
 
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
