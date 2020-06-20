04aa51cc46c4bcf358e360afd9324c5c572531f6
Ataru Oniki <ataru19980606@gmail.com>
2020-06-20 21:06:59 +0900

    update items#delete
    set_itemでitemを受け取るよう変更されていましたが、deleteの方が変更されていなかったため修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -79,7 +79,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == item.user_id && item.destroy
+    if current_user.id == @item.user_id && @item.destroy
       redirect_to root_path
     else
       redirect_to item_path(item)
