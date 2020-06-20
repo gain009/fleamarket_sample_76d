0a704f571767c97c03fdd31750297d585eda3f15
Ataru Oniki <ataru19980606@gmail.com>
2020-06-20 22:35:05 +0900

    remove unnecessary text
    式展開する必要がなかったため削除しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
     #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
 
